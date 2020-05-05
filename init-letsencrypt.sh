2037290175e1f8d80e7b5951b86309f4cab8953a
Survey Analyser <analysisers@gmail.com>
2020-05-05 12:59:47 +0200

    sonder naakte ip adres

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(datadudes.xyz www.datadudes.xyz localhost 165.227.107.150)
+domains=(datadudes.xyz www.datadudes.xyz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="cjp@gmail.com" # Adding a valid address is strongly recommended
