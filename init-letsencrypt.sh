524a8aa2b29e3b4754cb8bb51e7c1a1e5d7cdf0f
Survey Analyser <analysisers@gmail.com>
2020-05-05 12:56:17 +0200

    nog domein-name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(datadudes.xyz)
+domains=(datadudes.xyz www.datadudes.xyz localhost 165.227.107.150:80)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="cjp@gmail.com" # Adding a valid address is strongly recommended
